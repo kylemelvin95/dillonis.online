c63aa4c221b40f9fc852bff5daf0547db631be61
Dillon Mulroy <dillon.mulroy@gmail.com>
2024-07-29 13:23:36 -0400

    fix hover

--- src/routes/main-menu.svelte
+++ src/routes/main-menu.svelte
@@ -15,30 +15,42 @@
 	<Ascii />
 	<p class="text-center font-semibold text-3xl">software engineer @ vercel</p>
 	<ul class="flex flex-col w-1/2 gap-2 mt-8">
-		<li class="flex flex-row gap-2 items-center justify-between">
-			<span class="flex flex-row gap-4"><Github />GitHub</span>ggh
-		</li>
-		<li class="flex flex-row gap-2 items-center justify-between">
-			<span class="flex flex-row gap-4"><TwitterIcon />Twitter/𝕏</span>gx
+		<li
+			class="group p-1 cursor-none hover:cursor-pointer flex flex-row gap-2 items-center justify-between hover:bg-[#313346]"
+		>
+			<span class="flex flex-row gap-4 items-center"><Github />GitHub</span>ggh
 		</li>
 		<li
-			class="p-1 cursor-none hover:cursor-pointer flex flex-row gap-2 items-center justify-between hover:bg-[#313346] hover:text-[#6441a5]"
+			class="group p-1 cursor-none hover:cursor-pointer flex flex-row gap-2 items-center justify-between hover:bg-[#313346]"
+		>
+			<span class="flex flex-row gap-4 items-center"><TwitterIcon />Twitter/𝕏</span>gx
+		</li>
+		<li
+			class="group p-1 cursor-none hover:cursor-pointer flex flex-row gap-2 items-center justify-between hover:bg-[#313346]"
 		>
 			<span class="flex flex-row gap-4 items-center"
-				><TwitchIcon /><span class="text-ctp-subtext0">Twitch</span></span
+				><TwitchIcon class="group-hover:text-[#6441a5]" />Twitch</span
 			>gt
 		</li>
-		<li class="flex flex-row gap-2 items-center justify-between">
-			<span class="flex flex-row gap-4"><YoutubeIcon />YoutTube</span>gyt
+		<li
+			class="group p-1 cursor-none hover:cursor-pointer flex flex-row gap-2 items-center justify-between hover:bg-[#313346]"
+		>
+			<span class="flex flex-row gap-4 items-center"><YoutubeIcon />YoutTube</span>gyt
 		</li>
-		<li class="flex flex-row gap-2 items-center justify-between">
-			<span class="flex flex-row gap-4"><LinkedinIcon />LinkedIn</span>gli
+		<li
+			class="group p-1 cursor-none hover:cursor-pointer flex flex-row gap-2 items-center justify-between hover:bg-[#313346]"
+		>
+			<span class="flex flex-row gap-4 items-center"><LinkedinIcon />LinkedIn</span>gli
 		</li>
-		<li class="flex flex-row gap-2 items-center justify-between">
-			<span class="flex flex-row gap-4"><MicVocalIcon />Conference Talks</span>gct
+		<li
+			class="group p-1 cursor-none hover:cursor-pointer flex flex-row gap-2 items-center justify-between hover:bg-[#313346]"
+		>
+			<span class="flex flex-row gap-4 items-center"><MicVocalIcon />Conference Talks</span>gct
 		</li>
-		<li class="flex flex-row gap-2 items-center justify-between">
-			<span class="flex flex-row gap-4"><MailPlusIcon />Contact Me</span>gcm
+		<li
+			class="group p-1 cursor-none hover:cursor-pointer flex flex-row gap-2 items-center justify-between hover:bg-[#313346]"
+		>
+			<span class="flex flex-row gap-4 items-center"><MailPlusIcon />Contact Me</span>gcm
 		</li>
 	</ul>
 </main>
